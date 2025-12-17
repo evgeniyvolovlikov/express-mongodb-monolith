7197a98bf9c81c25e26ecfebe69ca575d9521f6a
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2025-12-17 04:04:31 +0300

    add and configure prettier

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app'
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`)
 })
